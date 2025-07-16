c09fdabf0459ce6abb9e59037988a4c21f1102f3
raghadabdullah-maker <raghadalhazmi280@gmail.com>
2025-07-16 21:32:38 +0300

    Fix API sign up URL for production

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import axios from 'axios'
 import { setTokens } from '../lib/api'
 
+
 function Signup() {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
